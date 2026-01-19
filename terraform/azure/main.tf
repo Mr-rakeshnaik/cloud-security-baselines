6750d7b47301b47b78b2d462ce7f6a504da98816
Rakesh Naik <49189455+Mr-rakeshnaik@users.noreply.github.com>
2026-01-19 04:51:52 +0000

    code fix

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -1,18 +1,31 @@
 provider "azurerm" {
   features {}
+  subscription_id = var.subscription_id
 }
 
-resource "azurerm_log_analytics_workspace" "security_logs" {
+# -----------------------------
+# Resource Group
+# -----------------------------
+resource "azurerm_resource_group" "security_rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# -----------------------------
+# Centralized Logging (Prevents AP-003)
+# -----------------------------
+resource "azurerm_log_analytics_workspace" "law" {
   name                = "law-security-baseline"
-  location            = "East US"
-  resource_group_name = "rg-security"
+  location            = azurerm_resource_group.security_rg.location
+  resource_group_name = azurerm_resource_group.security_rg.name
   sku                 = "PerGB2018"
+  retention_in_days   = 30
 }
 
-resource "azurerm_monitor_diagnostic_setting" "activity_logs" {
-  name                       = "activity-logs"
+resource "azurerm_monitor_diagnostic_setting" "subscription_logs" {
+  name                       = "subscription-activity-logs"
   target_resource_id         = "/subscriptions/${var.subscription_id}"
-  log_analytics_workspace_id = azurerm_log_analytics_workspace.security_logs.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
 
   log {
     category = "Administrative"
@@ -23,4 +36,32 @@ resource "azurerm_monitor_diagnostic_setting" "activity_logs" {
     category = "Security"
     enabled  = true
   }
+
+  log {
+    category = "Policy"
+    enabled  = true
+  }
+}
+
+# -----------------------------
+# IAM Guardrail (Mitigates AP-001)
+# -----------------------------
+resource "azurerm_role_definition" "limited_operator" {
+  name        = "Limited-Security-Operator"
+  scope       = "/subscriptions/${var.subscription_id}"
+  description = "Restricted role enforcing least privilege"
+
+  permissions {
+    actions = [
+      "Microsoft.Resources/subscriptions/resourceGroups/read",
+      "Microsoft.Compute/virtualMachines/read"
+    ]
+    not_actions = [
+      "Microsoft.Authorization/*"
+    ]
+  }
+
+  assignable_scopes = [
+    "/subscriptions/${var.subscription_id}"
+  ]
 }
---
+++ terraform/azure/variables.tf
@@ -0,0 +1,16 @@
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "East US"
+}
+
+variable "resource_group_name" {
+  description = "Security baseline resource group"
+  type        = string
+  default     = "rg-security-baseline"
+}
